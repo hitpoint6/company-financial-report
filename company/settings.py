b4366afa3a04b53ce4e01ced732a632708484b20
Guanghui He <hitpoint6@gmail.com>
2023-08-14 22:19:40 -0700

    Allow all origin

--- company/settings.py
+++ company/settings.py
@@ -133,7 +133,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://company-financial-report-backend.vercel.app/",
-]
+CORS_ALLOW_ALL_ORIGINS = True
